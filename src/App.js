59e9d308e313bbff3beb0cb22187b078a6f7023d
Sl1zz <o0stevenrogers0o@outlook.com>
2024-11-09 16:19:29 +1300

    deployed hopefully

---
+++ src/App.js
@@ -0,0 +1,32 @@
+// Import dependencies, other components
+import Header from "./my-components/Header"; //Header component
+import Footer from "./my-components/Footer";//Footer Component
+import Home from "./my-components/Home";// Home component
+import AdvancedJS from "./my-components/AdvancedJS";// Advanced JS
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import FAQ from './my-components/FAQ';//import FAQ Component
+import Invoice from './my-components/Invoice';//import Invoice Component
+
+// Functional component "App"
+function App() {
+
+  // UI component: HTML rendering
+  return (
+    <> {/* React Fragment: serves as parent component in JSX */}
+      
+      <Router>
+        <Header /> {/* Include Header component */}
+        <Routes>
+          <Route path="/" element={<Home />} /> {/* Home component*/}
+          <Route path="/advancedJS" element={<AdvancedJS />} /> {/* AdvancedJS component*/}
+          <Route path="/faq" element={<FAQ />}   />
+          <Route path="/invoice" element={<Invoice />}   />
+        </Routes>
+        <Footer /> {/*Include Footer component */}
+      </Router>
+    </>
+  );
+}
+
+// Export this component to the entire app
+export default App;
