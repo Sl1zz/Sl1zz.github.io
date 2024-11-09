5ca2923f242e5e41c1eaf20bc4c5b4d4e3995681
Sl1zz <o0stevenrogers0o@outlook.com>
2024-11-09 20:12:09 +1300

    dgrdf

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Header from "./my-components/Header"; //Header component
 import Footer from "./my-components/Footer";//Footer Component
 import Home from "./my-components/Home";// Home component
 import AdvancedJS from "./my-components/AdvancedJS";// Advanced JS
-import { BrowserRouter as  BrowserRouter, HashRouter as HashRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter as BrowserRouter, HashRouter as HashRouter, Routes, Route } from "react-router-dom";
 import FAQ from './my-components/FAQ';//import FAQ Component
 import Invoice from './my-components/Invoice';//import Invoice Component
 
@@ -13,19 +13,19 @@ function App() {
   // UI component: HTML rendering
   return (
     <> {/* React Fragment: serves as parent component in JSX */}
-      
-      {/* <BrowserRouter basename='/React-Project2'> */ }
+
+      {/* <BrowserRouter basename='/React-Project2'> */}
       <HashRouter>
         <Header /> {/* Include Header component */}
         <Routes>
           <Route path="/Home" element={<Home />} /> {/* Home component*/}
           <Route path="/AdvancedJS" element={<AdvancedJS />} /> {/* AdvancedJS component*/}
-          <Route path="/faq" element={<FAQ />}   />
-          <Route path="/invoice" element={<Invoice />}   />
+          <Route path="/faq" element={<FAQ />} />
+          <Route path="/invoice" element={<Invoice />} />
         </Routes>
         <Footer /> {/*Include Footer component */}
       </HashRouter>
-        { /*</BrowserRouter> */}
+      { /*</BrowserRouter> */}
     </>
   );
 }
